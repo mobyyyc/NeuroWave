4b15decdaaa14f1579135efdd13b6911ae413656
Qiyuan Cai <aly.moby@gmail.com>
2026-05-06 16:53:28 -0400

    Added ADSR / Filters

--- synth.py
+++ synth.py
@@ -1,8 +1,10 @@
 import numpy as np
 import soundfile as sf
-from scipy.signal import sawtooth, square
+from scipy.signal import sawtooth, square, butter, lfilter
+from patches import PATCHES
 
 SR = 44100
+
 
 def adsr(length, attack=0.01, decay=0.2, sustain=0.7, release=0.3):
     n = int(length * SR)
@@ -16,17 +18,27 @@ def adsr(length, attack=0.01, decay=0.2, sustain=0.7, release=0.3):
         env[:a] = np.linspace(0, 1, a)
 
     if d > 0:
-        env[a:a+d] = np.linspace(1, sustain, d)
+        env[a:a + d] = np.linspace(1, sustain, d)
 
     if r > 0:
         env[-r:] = np.linspace(sustain, 0, r)
 
     return env
 
+
+def lowpass_filter(audio, cutoff=2000):
+    nyquist = 0.5 * SR
+    normal_cutoff = cutoff / nyquist
+
+    b, a = butter(4, normal_cutoff, btype="low")
+    return lfilter(b, a, audio)
+
+
 def render_patch(
     freq=261.63,
     length=1.5,
     wave="sine",
+    cutoff=1200,
     level=0.8,
     attack=0.01,
     decay=0.2,
@@ -44,13 +56,21 @@ def render_patch(
     else:
         raise ValueError("Unknown wave type")
 
+    osc = lowpass_filter(osc, cutoff=cutoff)
+
     env = adsr(length, attack, decay, sustain, release)
     audio = osc * env * level
 
     audio = audio / np.max(np.abs(audio)) * 0.8
+
     return audio
 
+
 if __name__ == "__main__":
-    audio = render_patch(wave="sine")
-    sf.write("test_sine.wav", audio, SR)
-    print("Saved test_sine.wav")
+    patch = PATCHES["dark_saw"]
+
+    audio = render_patch(**patch)
+
+    sf.write("dark_saw.wav", audio, SR)
+
+    print("Saved dark_saw.wav")
